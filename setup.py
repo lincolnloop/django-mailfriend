41786c5ed84341c9028d23cf65db6e8ec5d28fbd
Peter Baumgartner <pete@lincolnloop.com>
2009-12-22 10:12:36 -0700

    Added setup.py and LICENSE/README

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='django-mailfriend',
+    version='1.0',
+    description='Generic "mail this to a friend" app for Django',
+    author='Jeffrey A. Croft',
+    url='http://code.google.com/p/django-mailfriend/',
+    packages=find_packages(),
+)
