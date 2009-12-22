921efbd346a99ffc308ecdaa44bf1349bf970c1f
Peter Baumgartner <pete@lincolnloop.com>
2009-12-22 15:28:51 -0700

    Removed hack from tests. Relying on the User model instead.

--- mailfriend/tests/__init__.py
+++ mailfriend/tests/__init__.py
@@ -1,37 +1,31 @@
-from django.test import TestCase
 from django.conf import settings
-from django.core import mail
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
-from django.core.management import call_command
-from django.db.models.loading import load_app
+from django.contrib.contenttypes.models import ContentType
+from django.core import mail
+from django.test import TestCase
 
-from mailfriend.views import mail_item_to_friend_send
-from fakeapp.models import FakeItem
+from mailfriend.models import MailedItem
 
 class MailfriendTest(TestCase):
     urls = 'mailfriend.tests.urls'
     
     def setUp(self):
-        # hack to temporarily setup a new app for testing
-        # see http://code.djangoproject.com/ticket/7835#comment:21
-        self.old_INSTALLED_APPS = settings.INSTALLED_APPS
-        settings.INSTALLED_APPS += (
-            'mailfriend.tests.fakeapp',
-        )
-        load_app('mailfriend.tests.fakeapp')
-        call_command('syncdb', verbosity=0, interactive=False) #Create tables for fakeapp
-        self.fake_item = FakeItem.objects.create(slug="mail-me")
-        self.fake_item_ct = ContentType.objects.get_for_model(self.fake_item)
-        User.objects.create_user('test', 'test_user@testserver.com', 'test')
+        self.user = User.objects.create_user('test', 'test_user@testserver.com', 'test')
+        self.user_ct = ContentType.objects.get_for_model(self.user)
 
-    def tearDown(self):
-        settings.INSTALLED_APPS = self.old_INSTALLED_APPS
+    def test_form_load(self):
+        """Form page loads"""
+        self.client.login(username='test', password='test')
+        response = self.client.get('/mailfriend/%s/%s/' % (self.user_ct.pk,
+                                                           self.user.pk))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mailfriend/form.html')
 
-    def test_anonymous_form(self):
+    def test_anonymous_send(self):
+        """Attempting to send as an anonymous user redirects to login"""
         response = self.client.post('/mailfriend/send/', {
-                        'content_type':str(self.fake_item_ct.pk),
-                        'object_id':str(self.fake_item.pk),
+                        'content_type':str(self.user_ct.pk),
+                        'object_id':str(self.user.pk),
                         'mailed_to':'test@testserver.com',
                         'user_email_as_from':'on',
                         'send_to_user_also':'on',
@@ -41,10 +35,11 @@ class MailfriendTest(TestCase):
                          'http://testserver/accounts/login/?next=/mailfriend/send/')
     
     def test_send_to_user(self):
+        """Sends an email and loads the correct page"""
         self.client.login(username='test', password='test')
         response = self.client.post('/mailfriend/send/', {
-                         'content_type':str(self.fake_item_ct.pk),
-                         'object_id':str(self.fake_item.pk),
+                         'content_type':str(self.user_ct.pk),
+                         'object_id':str(self.user.pk),
                          'mailed_to':'test@testserver.com',
                          'user_email_as_from':'on',
                          'send_to_user_also':'on',
@@ -54,3 +49,5 @@ class MailfriendTest(TestCase):
         self.assertEquals(len(mail.outbox), 1)
         self.assertEquals(mail.outbox[0].to,
                           [u'test@testserver.com', u'test_user@testserver.com'])
+        mailed_item = MailedItem.objects.all()[0]
+        self.assertEquals(mailed_item.mailed_to, 'test@testserver.com')
